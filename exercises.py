4d2d7ac89e98cc4508cf486ee10a88bf2e840a06
xabdulla <abdullashaker002@gmail.com>
2025-08-19 09:56:31 +0300

    all answers

--- exercises.py
+++ exercises.py
@@ -29,8 +29,8 @@ example_list_function()
 
 def manage_students():
     students = ['Ali', 'Sara', 'Khalid']
-    first_student = students[1]
-    last_student = students[-1]
+    first_student = students[0]
+    last_student = students[2]
     return first_student, last_student
 
 # Call the function and print the result
@@ -64,13 +64,62 @@ print('Exercise 2:', combine_foods())
 
 # Exercise 3: Slicing Tuples
 #
-# Using the slice operator, assign a new tuple containing only the last two food strings in the foods to a variable named last_two_foods.
+# Using the slice operator, assign a new tuple containing only the last two food 
+# strings in the foods to a variable named last_two_foods.
 
 def slice_foods():
-     foods = ("Pizza", "Burger", "Pasta", "Salad")
+     foods = ("Pizza", "Burger", "Pasta")
      last_two_foods = foods[-2:]
      return last_two_foods
 
 # Call the function and print the result
 print('Exercise 3:', slice_foods())
+
+##################################
+
+# Exercise 4: Dictionaries and String Formatting
+#
+# Create a dictionary named home_town containing the 
+# keys of city, state, and population.
+# Using the home_town dictionary, assign to a variable
+#  named home_town_message a string with this format: 
+# “I was born in <city>, <state> - population of <population>”
+
+
+
+
+
+def hometown_info():
+    home_town = {
+        "city": "Manama",
+        "state": "Capital",
+        "population": 200000
+    }
+
+    home_town_message = (
+        "I was born in " + home_town["city"] + ", "
+        + home_town["state"] + " - population of "
+        + str(home_town["population"])
+    )
+
+    return home_town_message
+
+print('Exercise 4:', hometown_info())
+
+############################################
+
+
+def list_home_town_items():
+    home_town = {
+        "city": "Manama",
+        "state": "Capital",
+        "population": 200000
+    }
+
+    home_town_items = []
+    for key, value in home_town.items():
+        home_town_items.append(f"{key} = {value}")
+    return home_town_items
+
+print('Exercise 5:', list_home_town_items())
 
